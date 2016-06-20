e162d29c5f2f5f69ad9e23f5de06ab85d0fe2fd6
Simer <simer@gmail.com>
2016-06-20 18:39:22 +0530

    session

--- app.js
+++ app.js
@@ -3,18 +3,36 @@
  * Module dependencies.
  */
 
-module.exports = function (flights,db) {
+module.exports = function (flights,dbconnection) {
 	var express = require('express');
+var MongoStore = require('connect-mongostore')(express);
+
+	var passport = require('./auth');
 	var routes = require('./routes')(flights);
 	var path = require('path');	
 	var app = express();
-
+console.log('mongoose.connection;-----'+dbconnection);
 	// all environments
 	app.set('port', process.env.PORT || 3000);
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'jade');
 	app.use(express.favicon());
 	app.use(express.logger('dev'));
+	app.use(express.cookieParser());
+	mongodb://simer:test1234@ds013564.mlab.com:13564/flights
+	app.use(express.session({
+		secret: 'keyboard cat',
+		store: new MongoStore({
+			  db: 'flights',
+			  host: 'ds013564.mlab.com',
+			  port: '13564',
+			  username: 'simer',
+			  password: 'test1234'
+ 
+		})
+	}));
+	app.use(passport.initialize());
+	app.use(passport.session());
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(function (req, res, next) {
@@ -34,7 +52,15 @@ module.exports = function (flights,db) {
 	app.get('/list', routes.list);
 	app.get('/arrivals', routes.arrivals);
 
+	app.get('/login', routes.login);
+	app.post('/login', passport.authenticate('local', {
+		failureRedirect: '/login',
+		successRedirect: '/user'
+	}));
+
+	app.get('/user', routes.user);
+
 	return app;
 }
-
+	 
 
--- db.js
+++ db.js
@@ -3,3 +3,4 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://simer:test1234@ds013564.mlab.com:13564/flights');
 
 module.exports = mongoose.connection;
+return mongoose.connection;
